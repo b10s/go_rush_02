b5984c0224401f9abb442fd5ceefbc4ce77575ee
Andrei Enshin <aenshin@c1r7s8.42tokyo.jp>
2024-07-07 21:33:42 +0900

    add check for invalid chars

--- ex00/vendor/piscine/validation.go
+++ ex00/vendor/piscine/validation.go
@@ -1,27 +1,32 @@
 package piscine
 
+//TODO: remove fmt
 import (
 	"os"
+	"fmt"
 )
 
 func Ft_ValidateInput(fileName string) bool {
-	_, err := os.ReadFile(fileName)
+	data, err := os.ReadFile(fileName)
 	if err != nil {
 		return false
 	}
 
-
-	return true
-}
-
+	// check if there is invalid char
+	fmt.Println(data)
+	for _, b := range data {
+		if b != 46 && b != 35 && b!= 10 {
+			return false
+		}
+	}
 
 // make check for new lines
-
-// make check only valid chars are here
 
 // make chekc shape is okay
 
 // make sure all tets are valid tets
 
-// make sure thera no more than 26 tets (maybe do it in sove since I can parse all tets there and easily fail there)
+	return true
+}
+
 
