0ab286400113a21a10fc781db70d1d0063473cb3
Andrei Enshin <aenshin@c1r7s8.42tokyo.jp>
2024-07-07 20:57:08 +0900

    merge other parts

--- ex00/main.go
+++ ex00/main.go
@@ -1,22 +1,17 @@
 package main
 
-// remove fmt from here
 import (
 	"os"
 	"piscine"
 )
 
 func main() {
-	// TODO: check if number of args is correct
-	//	if not, print usage
 	if len(os.Args) != 2 {
 		piscine.Ft_PutUsage()
 		return
 	}
 
-	// TODO: validate
 	fileName := os.Args[1]
-	piscine.Ft_PutStrLn(fileName)
 	if !piscine.Ft_ValidateInput(fileName) {
 		piscine.Ft_PutStrLn("error")
 		return
