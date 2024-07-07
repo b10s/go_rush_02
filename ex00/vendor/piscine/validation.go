7e73fa9a38f0ca1b1ddf44871825d623329e7b73
Andrei Enshin <aenshin@c1r7s8.42tokyo.jp>
2024-07-07 22:40:33 +0900

    last check is done

--- ex00/vendor/piscine/validation.go
+++ ex00/vendor/piscine/validation.go
@@ -21,6 +21,9 @@ func Ft_ValidateInput(fileName string) bool {
 	}
 
 	// check if there is invalid char
+	// 10 - end of line
+	// 35 - #
+	// 46 - .
 	for _, b := range data {
 		if b != 46 && b != 35 && b!= 10 {
 			return false
@@ -88,8 +91,91 @@ func Ft_ValidateInput(fileName string) bool {
 	}
 
 	// make sure all tets are valid tets
+	// sum of each tet neighbors should be 6: no less, no more
+	tet := [][]byte{}
+	line := []byte{}
+	pre = 42
+	for _, b := range data {
+		if b == 10 {
+			// empty line, tet completed
+			// check tet
+			if pre == 10 {
+				if countBlocks(tet) != 4 {
+					return false
+				}
+				if countAdjBlcks(tet) < 6 {
+					return false
+				}
+				tet = [][]byte{}
+				line = []byte{}
+			// just another line of tet
+			// keep building
+			} else {
+				tet = append(tet, line)
+				line = []byte{}
+			}
+		} else {
+			line = append(line, b)
+		}
+		pre = b
+	}
+
+	//check last tet
+	if countBlocks(tet) != 4 {
+		return false
+	}
+	if countAdjBlcks(tet) < 6 {
+		return false
+	}
+
 
 	return true
 }
 
 
+func countBlocks(tet [][]byte) int {
+	res := 0
+	for i:=0; i<len(tet); i++ {
+		for j:=0; j<len(tet[i]); j++ {
+			if tet[i][j] == 35 {
+				res++
+			}
+		}
+	}
+	return res
+}
+
+func countAdjBlcks(tet [][]byte) int {
+	res := 0
+	for i:=0; i<len(tet); i++ {
+		for j:=0; j<len(tet[i]); j++ {
+			if tet[i][j] == 35 {
+				// upper block
+				if i - 1 >=0 {
+					if tet[i-1][j] == 35 {
+						res++
+					}
+				}
+				// lower block
+				if i + 1 < len(tet) {
+					if tet[i+1][j] == 35 {
+						res++
+					}
+				}
+				// left block
+				if j - 1 >= 0 {
+					if tet[i][j-1] == 35 {
+						res++
+					}
+				}
+				// right block
+				if j+ 1 < len(tet[i]) {
+					if tet[i][j+1] == 35 {
+						res++
+					}
+				}
+			}
+		}
+	}
+	return res
+}
